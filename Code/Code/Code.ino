02332c6c14887fca3219551051eaef01a959dbc5
BenjiChejtman <46895375@est.ort.edu.ar>
2023-06-07 13:41:26 -0300

    .

--- Code/Code/Code.ino
+++ Code/Code/Code.ino
@@ -1,10 +1,37 @@
+//------archivos
+
+#include <SPI.h>
+#include <WiFi.h>
+#include <ESP8266WiFi.h>
+
+
+//------codigo: setup
+
+int parada = 0;
+
+WiFiServer server (80);
 void setup() {
   // put your setup code here, to run once:
-
-  Serial.Begin(
+  Serial.begin(9600);
 }
+
+//------codigo: bucle
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  while (parada >= 0)
+  {
+    if (parada == 0)
+    {
+      Serial.println("X");
+    }
+      
+    if (parada == 1)
+    {
+       parada = analogRead(A0);
+      Serial.println("¡Persona esperando en parada!");
+      Serial.println(parada);
+      delay(500);
+    }
+  }
 }
